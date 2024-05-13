17d6736acb7a90373c366240000ebffd5425cfcb
tongyf <tongyf@meow.com>
2024-05-13 21:51:45 +0800

    commit

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     static char line[4096];
+    int cnt=0;
+    __pid_t pid;
+
+    FILE *fp;
+    fp=fopen("/tmp/code.c","a");
+    fclose(fp);
+    char *args[]={"rm", "/tmp/code.c", NULL};
+    pid=fork();
+    if(!pid){
+        execvp("rm",args);
+        exit(0);
+    }
+    fp=fopen("/tmp/code.so","a");
+    fclose(fp);
+    char *args1[]={"rm", "/tmp/code.so", NULL};
+    pid=fork();
+    if(!pid){
+        execvp("rm",args1);
+        exit(0);
+    }
 
     while (1) {
         printf("crepl> ");
@@ -11,8 +34,56 @@ int main(int argc, char *argv[]) {
         if (!fgets(line, sizeof(line), stdin)) {
             break;
         }
-
-        // To be implemented.
+        if(line[0]=='i'&&line[1]=='n'&&line[2]=='t'){
+            FILE *fp;
+            fp=fopen("/tmp/code.c","a");
+            fprintf(fp,"%s\n",line);
+            fclose(fp);
+            char *args[]={"gcc", "-shared", "-fPIC", "-o", "/tmp/code.so", "/tmp/code.c", NULL};
+            pid=fork();
+            if(!pid){
+                execvp("gcc",args);
+                exit(0);
+            }
+        }
+        else{
+            cnt++;
+            char func[4296];
+            char name[100];
+            sprintf(name,"eval%d",cnt);
+            sprintf(func,"int %s(){return %s;}",name,line);
+            FILE *fp;
+            fp=fopen("/tmp/code.c","a");
+            fprintf(fp,"%s\n",func);
+            fclose(fp);
+            char *args[]={"gcc", "-shared", "-fPIC", "-o", "/tmp/code.so", "/tmp/code.c", NULL};
+            pid=fork();
+            if(!pid){
+                execvp("gcc",args);
+                exit(0);
+            }
+            printf("QAQ\n");
+            void *handle;
+            int (*foo)(void);
+            char *error;
+            handle = dlopen("/tmp/code.so", RTLD_LAZY);
+            if (!handle) {
+                fprintf(stderr, "%s\n", dlerror());
+                return 1;
+            }
+            dlerror();
+            printf("QAQ\n");
+            foo=dlsym(handle, name);
+            printf("QAQ\n");
+            if ((error = dlerror()) != NULL)  {
+                fprintf(stderr, "%s\n", error);
+                dlclose(handle);
+                return 1;
+            }
+            printf("QAQ\n");
+            printf("%d\n",foo());
+            dlclose(handle);
+        }
         printf("Got %zu chars.\n", strlen(line));
     }
 }
