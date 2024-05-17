dea7baaecc2931f609df69307b290d597d33f6ba
tracer-nju <tracer@nju.edu.cn>
2024-05-18 00:09:55 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:09:55 up  5:45,  1 user,  load average: 0.26, 0.23, 0.20

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -7,8 +7,49 @@
 
 double tim[4096];
 int tot;
+char cur[4096];
 
-void parseNumbersInAngleBrackets(const char *str) {
+typedef struct node{
+    char *name;
+    double time;
+}node;
+node table[10005],temp[10005];
+int cnt;
+int cmp(node a,node b){
+    return a.time>b.time;
+}
+void add(const char *str,double val){
+    printf("QAQ\n");
+    int find=0;
+    for(int i=1;i<=cnt;i++){
+        if(strcmp(str,table[i].name)==0){
+            table[i].time+=val;
+            find=1;
+            break;
+        }
+    }
+    if(!find){
+        table[++cnt].name=(char *)str;
+        table[cnt].time=val;
+    }
+}
+void merge(int l,int r){
+	if(l==r) return;
+	int mid=(l+r)/2;
+	merge(l,mid);
+	merge(mid+1,r);
+	int hd=l,tl=mid+1,now=l;
+	while(hd<=mid&&tl<=r){
+		if(cmp(table[hd],table[tl])) temp[now++]=table[hd++];
+		else temp[now++]=table[tl++];
+	}
+	while(hd<=mid) temp[now++]=table[hd++];
+	while(tl<=r) temp[now++]=table[tl++];
+	for(int i=l;i<=r;i++) table[i]=temp[i];
+}
+
+
+void gettim(const char *str) {
     char bf[4096];
     regex_t regex;
     int reti;
@@ -21,7 +62,28 @@ void parseNumbersInAngleBrackets(const char *str) {
         strncpy(bf, str+mch[i].rm_so, mch[i].rm_eo-mch[i].rm_so);
         bf[mch[i].rm_eo-mch[i].rm_so]=0;
         double db=atof(bf);
-        if(db!=0.0) tim[++tot]=db;
+        if(db!=0.0){
+            tim[++tot]=db;
+            add(cur,tim[tot]);
+        }
+    }
+    regfree(&regex);
+}
+
+void getname(const char *str){
+    regex_t regex;
+    int reti;
+    int match=2;
+    regmatch_t mch[5];
+    printf("QAQ\n");
+    reti = regcomp(&regex, "^\\w+(?=\\()", REG_EXTENDED);
+    printf("QAQ\n");
+    reti = regexec(&regex, str, match, mch, 0);
+    if(reti==REG_NOMATCH) return;
+    for (int i=0;i<match&&mch[i].rm_so!=-1;i++){
+        printf("QAQ\n");
+        strncpy(cur, str+mch[i].rm_so, mch[i].rm_eo-mch[i].rm_so);
+        //printf("name:%s\n",cur);
     }
     regfree(&regex);
 }
@@ -53,14 +115,16 @@ int main(int argc, char *argv[]) {
         ssize_t bytesRead;
         while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
             write(STDOUT_FILENO, buffer, bytesRead);
-            parseNumbersInAngleBrackets(buffer);
+            printf("QAQ\n");
+            getname(buffer);
+            printf("QAQ\n");
+            gettim(buffer);
             memset(buffer,0,sizeof(buffer));
-            //printf("\n");
             fflush(stdout);
         }
         close(pipefd[0]);
-        printf("%d\n",tot);
-        for(int i=1;i<=tot;i++) printf("%lf\n",tim[i]);
+        printf("%d\n",cnt);
+        for(int i=1;i<=cnt;i++) printf("%s %lf\n",table[i].name,table[i].time);
     }
     return 0;
 }
