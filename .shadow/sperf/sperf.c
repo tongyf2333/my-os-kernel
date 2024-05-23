c69761425885ded34b2801748022352828a7bc83
tracer-nju <tracer@nju.edu.cn>
2024-05-23 21:58:20 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:58:20 up  1:07,  1 user,  load average: 0.24, 0.29, 0.89

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -11,13 +11,13 @@ const char *nul="\0";
 const double eps=1e-9;
 double sum,curtime,lasttime=0.0;
 int tot;
-char cur[4096];
+char cur[40096];
 
 typedef struct node{
     char *name;
     double time;
 }node;
-node table[10005],temp[10005];
+node table[100005],temp[100005];
 int cnt=0;
 int cmp(node a,node b){
     return a.time-b.time>eps;
@@ -55,7 +55,7 @@ void merge(int l,int r){
 
 
 void gettim(const char *str) {
-    char bf[4096];
+    char bf[40096];
     regex_t regex;
     int reti;
     int match=2;
@@ -75,7 +75,7 @@ void gettim(const char *str) {
 }
 
 void getname(const char *str){
-    char bf[4096];
+    char bf[40096];
     regex_t regex;
     int reti;
     int match=2;
@@ -130,7 +130,7 @@ int main(int argc, char *argv[]) {
     }
     else{
         close(pipefd[1]);
-        char buffer[4096];
+        char buffer[40096];
         ssize_t bytesRead;
         while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
             //write(STDOUT_FILENO,buffer,bytesRead);
