9edbcbdefd9d550b5f301009d54e0093bbe35c55
tongyf <tongyf@meow.com>
2024-06-10 21:55:52 +0800

    commit

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -21,7 +21,17 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for(;*s1 == *s2; s1++, s2++){	
+		if (*s1 == '\0')
+		return 0;
+	}
+	if(*(unsigned char*)s1 > *(unsigned char*)s2){
+		return 1;
+	}
+	else if(*(unsigned char*)s1 < *(unsigned char*)s2){
+		return -1;
+	}
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +39,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  const unsigned char uc = c;
+  unsigned char *su;
+  for(su = s;0 < n;++su,--n)
+    *su = uc;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
