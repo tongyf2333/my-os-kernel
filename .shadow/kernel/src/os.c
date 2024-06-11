216b0985f05af151245a88a94403500220ba9b79
tracer-nju <tracer@nju.edu.cn>
2024-06-11 22:30:31 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:31 up 2 days, 46 min,  1 user,  load average: 0.61, 0.47, 0.39

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,4 +1,6 @@
 #include <common.h>
+#define INT_MIN -2147483647
+#define INT_MAX 2147483647
 sem_t empty, fill;
 #define P kmt->sem_wait
 #define V kmt->sem_signal
@@ -13,33 +15,6 @@ static inline task_t *task_alloc() {
 
 extern task_t *tasks[],*current_task;
 
-static void os_init() {
-    pmm->init();
-    kmt->init();
-    dev->init();
-    printf("init finished\n");
-    //kmt->create(task_alloc(),"_",NULL,NULL);
-    kmt->sem_init(&empty, "empty", N);
-    kmt->sem_init(&fill,  "fill",  0);
-    for (int i = 0; i < NPROD; i++) {
-        kmt->create(task_alloc(), "producer", Tproduce, NULL);
-    }
-    for (int i = 0; i < NCONS; i++) {
-        kmt->create(task_alloc(), "consumer", Tconsume, NULL);
-    }
-}
-
-static void os_run() {
-    //printf("os_run\n");
-    //iset(true);
-    //printf("iset true\n");
-    yield();
-    //printf("over\n");
-    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-        putch(*s == '*' ? '0' + cpu_current() : *s);
-    }
-    while (1) ;
-}
 
 typedef struct hand{
     int seq,event;
@@ -68,34 +43,33 @@ void merge(int l,int r){
 	for(int i=l;i<=r;i++) table[i]=temp[i];
 }
 
+static Context *kmt_context_save(Event ev, Context *ctx){
+    if (!current_task) current_task = tasks[0];
+    else current_task->context = ctx;
+    return current_task->context;
+}
+static Context *kmt_schedule(Event ev, Context *ctx){
+    do {
+        current_task = current_task->next;
+    } while (
+        (current_task - tasks[0]) % cpu_count() != cpu_current()||
+        current_task->status != RUNNING
+    );
+    current_task=current_task->next;
+    current_task->status=RUNNING;
+    return current_task->context;
+}
 static Context *os_trap(Event ev, Context *ctx){
-    if(ev.event==EVENT_YIELD){
-        if (!current_task) current_task = tasks[0];
-        else current_task->context = ctx;
-        do {
-            current_task = current_task->next;
-        } while (
-            (current_task - tasks[0]) % cpu_count() != cpu_current()||
-            current_task->status != RUNNING
-        );
-        current_task=current_task->next;
-        current_task->status=RUNNING;
-        return current_task->context;
-    }
-    else{
-        //if (!current_task) current_task = tasks[0];
-        //else current_task->context = ctx;
-        Context *next = NULL;
-        for (int i=1;i<=cnt;i++) {
-            hand h=table[i];
-            if (h.event == EVENT_NULL || h.event == ev.event) {
-                Context *r = h.handler(ev, ctx);
-                if (r) next = r;
-            }
+    Context *next = NULL;
+    for (int i=1;i<=cnt;i++) {
+        hand h=table[i];
+        if (h.event == EVENT_NULL || h.event == ev.event) {
+            Context *r = h.handler(ev, ctx);
+            if (r) next = r;
         }
-        panic_on(!next, "return to NULL context");
-        return next;
     }
+    panic_on(!next, "return to NULL context");
+    return next;
 }
 
 static void os_on_irq(int seq, int event, handler_t handler){
@@ -103,6 +77,36 @@ static void os_on_irq(int seq, int event, handler_t handler){
     table[cnt].handler=handler;
     table[cnt].seq=seq;
     merge(1,cnt);
+}
+
+static void os_init() {
+    pmm->init();
+    kmt->init();
+    os->on_irq(INT_MIN,EVENT_NULL,kmt_context_save);
+    os->on_irq(INT_MAX,EVENT_NULL,kmt_schedule);
+    dev->init();
+    printf("init finished\n");
+    //kmt->create(task_alloc(),"_",NULL,NULL);
+    kmt->sem_init(&empty, "empty", N);
+    kmt->sem_init(&fill,  "fill",  0);
+    for (int i = 0; i < NPROD; i++) {
+        kmt->create(task_alloc(), "producer", Tproduce, NULL);
+    }
+    for (int i = 0; i < NCONS; i++) {
+        kmt->create(task_alloc(), "consumer", Tconsume, NULL);
+    }
+}
+
+static void os_run() {
+    //printf("os_run\n");
+    //iset(true);
+    //printf("iset true\n");
+    yield();
+    //printf("over\n");
+    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+        putch(*s == '*' ? '0' + cpu_current() : *s);
+    }
+    while (1) ;
 }
 
 MODULE_DEF(os) = {
