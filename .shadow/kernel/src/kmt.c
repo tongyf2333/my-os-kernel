ddc04b9b264bb7a84e18dde87cca4459c3d4c66b
tracer-nju <tracer@nju.edu.cn>
2024-06-12 23:33:29 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:29 up  3:25,  1 user,  load average: 0.97, 0.28, 0.14

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -88,7 +88,7 @@ static void kmt_sem_init(sem_t *sem, const char *name, int value){
 
 }
 static void kmt_sem_wait(sem_t *sem){
-    printf("P:%s at cpu:%d\n",sem->lk->name,cpu_current()+1);
+    //printf("P:%s at cpu:%d\n",sem->lk->name,cpu_current()+1);
     //kmt_spin_lock(sem->lk);
     int flag=0;
     if(sem->count<=0){
@@ -101,7 +101,7 @@ static void kmt_sem_wait(sem_t *sem){
     if(flag==1) yield();
 }
 static void kmt_sem_signal(sem_t *sem){
-    printf("V:%s at cpu:%d\n",sem->lk->name,cpu_current()+1);
+    //printf("V:%s at cpu:%d\n",sem->lk->name,cpu_current()+1);
     //kmt_spin_lock(sem->lk);
     sem->count++;
     if((sem->que->hd)<=(sem->que->tl)) {
