31eee87affcc874d233eae0febadf961c8b747e7
tracer-nju <tracer@nju.edu.cn>
2024-06-12 17:49:31 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:31 up 2 days,  9:19,  1 user,  load average: 0.33, 0.21, 0.17

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -86,6 +86,9 @@ static void os_init() {
 }
 
 static void os_run() {
+    for (const char *s = "inside CPU #*\n"; *s; s++) {
+        putch(*s == '*' ? '0' + cpu_current() : *s);
+    }
     kmt->sem_init(&empty, "empty", N);
     kmt->sem_init(&fill,  "fill",  0);
     for (int i = 0; i < NPROD; i++) {
