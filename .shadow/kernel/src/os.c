68ffddc23a38d515d5ca8db9d93bb963cf6d4407
tracer-nju <tracer@nju.edu.cn>
2024-06-13 17:35:32 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:35:32 up  6:12,  1 user,  load average: 0.64, 0.53, 0.41

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -64,7 +64,7 @@ static Context *kmt_schedule(Event ev, Context *ctx){
         /*current_task->cpu_id != cpu_current() ||*/
         current_task->status != RUNNING 
     );
-    printf("at:%d",current_task->id+1);
+    //printf("at:%d",current_task->id+1);
     return current_task->context;
 }
 static Context *os_trap(Event ev, Context *ctx){
