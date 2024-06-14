3cb658a6a25e52a65fba0881347b31398be3333a
tracer-nju <tracer@nju.edu.cn>
2024-06-14 11:36:17 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:36:17 up 13:25,  1 user,  load average: 0.32, 0.17, 0.27

--- .shadow/kernel/src/dev/dev_input.c
+++ .shadow/kernel/src/dev/dev_input.c
@@ -189,6 +189,7 @@ void dev_input_task(void *args) {
     uint32_t time;
     AM_INPUT_KEYBRD_T key;
     while ((key = io_read(AM_INPUT_KEYBRD)).keycode != 0) {
+      printf("QWQ\n");
       input_keydown(in, key);
     }
     time = io_read(AM_TIMER_UPTIME).us;
