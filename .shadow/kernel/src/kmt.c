7ac9ca3a98708371b42d1c453e823595aee5faf7
tracer-nju <tracer@nju.edu.cn>
2024-06-14 23:17:15 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:17:15 up 1 day,  1:06,  1 user,  load average: 0.85, 0.90, 0.87

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -114,7 +114,7 @@ static void kmt_sem_init(sem_t *sem, const char *name, int value){
     sem->que->cnt=0;
 }
 static void kmt_sem_wait(sem_t *sem){
-    int acquired=0;
+    /*int acquired=0;
     kmt_spin_lock(sem->lk);
     if (sem->count<=0) {
         current_task[cpu_current()]->status = BLOCKED;
@@ -126,22 +126,35 @@ static void kmt_sem_wait(sem_t *sem){
         acquired = 1;
     }
     kmt_spin_unlock(sem->lk);
-    if (!acquired) yield();
-    /*kmt_spin_lock(sem->lk);
+    if (!acquired) yield();*/
+    kmt_spin_lock(sem->lk);
     sem->count--;
     if(sem->count<0){
-        enqueue(sem->que, current_task[cpu_current()]);
-        current_task[cpu_current()]->status = BLOCKED;
+        task_t *now=current_task[cpu_current()];
+        now->status = BLOCKED;
+        enqueue(sem->que, now);
+        while(now->status==BLOCKED){
+            kmt_spin_unlock(sem->lk);
+            yield();
+            kmt_spin_lock(sem->lk);
+        }
     }
-    kmt_spin_unlock(sem->lk);*/
+    kmt_spin_unlock(sem->lk);
 }
 static void kmt_sem_signal(sem_t *sem){
-    kmt_spin_lock(sem->lk);
+    /*kmt_spin_lock(sem->lk);
     if((sem->que->cnt)>0) {
         task_t *task = dequeue(sem->que);
         task->status = RUNNING;
     } 
     else sem->count++;
+    kmt_spin_unlock(sem->lk);*/
+    kmt_spin_lock(sem->lk);
+    sem->count++;
+    if(sem->count<=0){
+        task_t *now=dequeue(sem->que);
+        if(now) now->status=RUNNING;
+    }
     kmt_spin_unlock(sem->lk);
 }
 
