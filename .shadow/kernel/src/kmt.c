37f871e7406c86817eab1ba041e178402d42d5d4
tracer-nju <tracer@nju.edu.cn>
2024-06-16 01:17:23 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:17:23 up 1 day, 14:37,  1 user,  load average: 0.94, 0.70, 0.56

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -133,23 +133,9 @@ static void kmt_sem_init(sem_t *sem, const char *name, int value){
     sem->que->cnt=0;
 }
 static void kmt_sem_wait(sem_t *sem){
-    int acquired=0;
     kmt_spin_lock(sem->lk);
-    if (sem->count<=0) {
-        task_t *now=current_task[cpu_current()];
-        now->status = BLOCKED;
-        enqueue(sem->que, now);
-    } 
-    else {
-        sem->count--;
-        acquired = 1;
-    }
-    kmt_spin_unlock(sem->lk);
-    if (!acquired) yield();
-    /*kmt_spin_lock(sem->lk);
     sem->count--;
     if(sem->count<0){
-        //printf("QWQ\n");
         task_t *now=current_task[cpu_current()];
         now->status = BLOCKED;
         enqueue(sem->que, now);
@@ -159,23 +145,12 @@ static void kmt_sem_wait(sem_t *sem){
             kmt_spin_lock(sem->lk);
         }
     }
-    kmt_spin_unlock(sem->lk);*/
+    kmt_spin_unlock(sem->lk);
 }
 static void kmt_sem_signal(sem_t *sem){
     kmt_spin_lock(sem->lk);
-    if(sem->que->cnt>0){
-        task_t *now=dequeue(sem->que);
-        now->status=RUNNING;
-        kmt_spin_lock(&lock);
-        enqueue(global,now);
-        kmt_spin_unlock(&lock);
-    }
-    else sem->count++;
-    kmt_spin_unlock(sem->lk);
-    /*kmt_spin_lock(sem->lk);
     sem->count++;
     if(sem->count<=0){
-        //printf("QAQ\n");
         if(sem->que->cnt>0){
             task_t *now=dequeue(sem->que);
             now->status=RUNNING;
@@ -184,7 +159,7 @@ static void kmt_sem_signal(sem_t *sem){
             kmt_spin_unlock(&lock);
         }
     }
-    kmt_spin_unlock(sem->lk);*/
+    kmt_spin_unlock(sem->lk);
 }
 
 static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg){
