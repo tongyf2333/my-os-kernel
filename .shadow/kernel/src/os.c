af14b20f9b6b1987fe65abca08d2b4b26020b708
tracer-nju <tracer@nju.edu.cn>
2024-06-16 18:43:20 +0800

    Linux tongyf-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:20 up 1 day, 23:36,  1 user,  load average: 0.66, 0.86, 0.77

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -57,7 +57,7 @@ static void os_on_irq(int seq, int event, handler_t handler){
     table[cnt].seq=seq;
     merge(1,cnt);
 }
-/*
+
 static void hard_test(){
     kmt->sem_init(&empty, "empty", N);
     kmt->sem_init(&fill,  "fill",  0);
@@ -68,7 +68,7 @@ static void hard_test(){
         kmt->create(task_alloc(), "consumer", Tconsume, NULL);
     }
 }
-*/
+
 /*
 static void easy_test(){
     kmt->spin_init(&lkk,"lkk");
@@ -78,9 +78,9 @@ static void easy_test(){
 static void os_init() {
     pmm->init();
     kmt->init();
-    dev->init();
+    //dev->init();
     //easy_test();
-    //hard_test();
+    hard_test();
 }
 static void os_run() {
     iset(true);
