eee8612a22718a49ffc61b6e3671417b88300a77
tongyf <tongyf@meow.com>
2024-07-04 01:12:20 +0800

    commit

--- fsrecov/fsrecov.c
+++ fsrecov/fsrecov.c
@@ -8,13 +8,10 @@
 #include "fat32.h"
 
 struct fat32hdr *hdr;
-
 wchar_t long_name[4096];
-
 LFNEntry *names[256];
-
-void *mmap_disk(const char *fname);
-void dfs_scan(u32 clusId, int depth, int is_dir);
+int *status;
+char buffer[8*1024*1024],buf[128],buffer1[4*1024*1024];
 
 uint8_t calc_checksum(uint8_t *sfn){
     uint8_t sum = 0;
@@ -24,21 +21,13 @@ uint8_t calc_checksum(uint8_t *sfn){
     return sum;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s fs-image\n", argv[0]);
-        exit(1);
-    }
-    setbuf(stdout, NULL);
-    assert(sizeof(struct fat32hdr) == 512);
-    assert(sizeof(struct fat32dent) == 32);
-    assert(sizeof(LFNEntry) == 32);
+void *cluster_to_sec(int n) {
+    // RTFM: Sec 3.5 and 4 (TRICKY)
+    // Don't copy code. Write your own.
 
-    hdr = mmap_disk(argv[1]);
-
-    dfs_scan(hdr->BPB_RootClus, 0, 1);
-
-    munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);
+    u32 DataSec = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32;
+    DataSec += (n - 2) * hdr->BPB_SecPerClus;
+    return ((char *)hdr) + DataSec * hdr->BPB_BytsPerSec;
 }
 
 void *mmap_disk(const char *fname) {
@@ -63,13 +52,13 @@ void *mmap_disk(const char *fname) {
     assert(hdr->Signature_word == 0xaa55); // this is an MBR
     assert(hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec == size);
 
-    printf("%s: DOS/MBR boot sector, ", fname);
+    /*printf("%s: DOS/MBR boot sector, ", fname);
     printf("OEM-ID \"%s\", ", hdr->BS_OEMName);
     printf("sectors/cluster %d, ", hdr->BPB_SecPerClus);
     printf("sectors %d, ", hdr->BPB_TotSec32);
     printf("sectors %d, ", hdr->BPB_TotSec32);
     printf("sectors/FAT %d, ", hdr->BPB_FATSz32);
-    printf("serial number 0x%x\n", hdr->BS_VolID);
+    printf("serial number 0x%x\n", hdr->BS_VolID);*/
     return hdr;
 
 release:
@@ -80,60 +69,51 @@ release:
     exit(1);
 }
 
-u32 next_cluster(int n) {
-    // RTFM: Sec 4.1
-
-    u32 off = hdr->BPB_RsvdSecCnt * hdr->BPB_BytsPerSec;
-    u32 *fat = (u32 *)((u8 *)hdr + off);
-    return fat[n];
-}
-
-void *cluster_to_sec(int n) {
-    // RTFM: Sec 3.5 and 4 (TRICKY)
-    // Don't copy code. Write your own.
-
-    u32 DataSec = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32;
-    DataSec += (n - 2) * hdr->BPB_SecPerClus;
-    return ((char *)hdr) + DataSec * hdr->BPB_BytsPerSec;
-}
-
-void get_filename(struct fat32dent *dent, char *buf) {
-    // RTFM: Sec 6.1
-
-    int len = 0;
-    for (int i = 0; i < sizeof(dent->DIR_Name); i++) {
-        if (dent->DIR_Name[i] != ' ') {
-            if (i == 8)
-                buf[len++] = '.';
-            buf[len++] = dent->DIR_Name[i];
-        }
+int getsha(char *begin){
+    const char *file_path = "/tmp/a.txt";
+    FILE *file = fopen(file_path, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
     }
-    buf[len] = '\0';
+    if (fprintf(file, "%s\n", begin) < 0) {
+        perror("Error writing to file");
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+    if (fclose(file) != 0) {
+        perror("Error closing file");
+        return EXIT_FAILURE;
+    }
+    FILE *fp=popen("sha1sum /tmp/a.txt","r");
+    fscanf(fp,"%s",buf);
+    pclose(fp);
+    buf[40]='\0';
+    printf("%s ",buf);
+    return 0;
 }
 
-void dfs_scan(u32 clusId, int depth, int is_dir) {
-    // RTFM: Sec 6
-
-    for (; clusId < CLUS_INVALID; clusId = next_cluster(clusId)) {
-
-        if (is_dir) {
+void solve(){
+    uint32_t first_data_sector = hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * hdr->BPB_FATSz32);
+    uint32_t total_clusters = (hdr->BPB_TotSec32 - first_data_sector) / hdr->BPB_SecPerClus;
+    status=malloc((total_clusters+1)*sizeof(int));
+    for (int clusId=2; clusId < total_clusters; clusId ++) {
+        int bmpcnt=0;
+        uint8_t *head=(uint8_t*)cluster_to_sec(clusId),*tl=(uint8_t*)cluster_to_sec(clusId+1);
+        for(uint8_t *now=head;now<tl;now++){
+            if(now+2>=tl) break;
+            if(*now=='B'&&*(now+1)=='M'&&*(now+2)=='P') bmpcnt++;
+        }
+        if(bmpcnt>=9){
             int ndents = hdr->BPB_BytsPerSec * hdr->BPB_SecPerClus / sizeof(struct fat32dent);
-
             for (int d = 0; d < ndents; d++) {
                 struct fat32dent *dent = (struct fat32dent *)cluster_to_sec(clusId) + d;
-                LFNEntry *dt=(LFNEntry*)cluster_to_sec(clusId)+d;
                 if (dent->DIR_Name[0] == 0x00 ||
                     dent->DIR_Name[0] == 0xe5 ||
                     dent->DIR_Attr & ATTR_HIDDEN)
                     continue;
 
-                char fname[32];
-                get_filename(dent, fname);
-
-                for (int i = 0; i < 4 * depth; i++)
-                    putchar(' ');
-
-                if(dt->attr!=0xf){
+                if(dent->DIR_Attr==0x20){
                     int cnt=0;
                     uint8_t checksum=calc_checksum(dent->DIR_Name);
                     for(int i=0;i<256;i++) names[i]=NULL;
@@ -153,25 +133,28 @@ void dfs_scan(u32 clusId, int depth, int is_dir) {
                         }
                     }
                     long_name[cnt++]='\0';
-                    printf("long name:%ls\n",long_name);
-                }
-
-                printf("[%-12s] %6.1lf KiB    ", fname, dent->DIR_FileSize / 1024.0);
-
-                u32 dataClus = dent->DIR_FstClusLO | (dent->DIR_FstClusHI << 16);
-                if (dent->DIR_Attr & ATTR_DIRECTORY) {
-                    
-                    printf("\n");
-                    if (dent->DIR_Name[0] != '.') {
-                        dfs_scan(dataClus, depth + 1, 1);
-                    }
-                } else {
-                    dfs_scan(dataClus, depth + 1, 0);
-                    printf("\n");
+                    u32 dataClus = dent->DIR_FstClusLO | (dent->DIR_FstClusHI << 16);
+                    char *pointer=cluster_to_sec(dataClus);
+                    strncpy(buffer1,pointer,dent->DIR_FileSize);
+                    getsha(buffer1);
+                    printf(" %ls\n",long_name);
                 }
             }
-        } else {
-            printf("#%d ", clusId);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s fs-image\n", argv[0]);
+        exit(1);
+    }
+    setbuf(stdout, NULL);
+    assert(sizeof(struct fat32hdr) == 512);
+    assert(sizeof(struct fat32dent) == 32);
+    assert(sizeof(LFNEntry) == 32);
+
+    hdr = mmap_disk(argv[1]);
+    solve();
+    munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);
 }
