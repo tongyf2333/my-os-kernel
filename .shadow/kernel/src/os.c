195e0e76cd8445ce565a283362f2184d6d773818
tracer-nju <tracer@nju.edu.cn>
2024-07-13 22:15:28 +0800

    Linux tongyf-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:28 up 27 min,  1 user,  load average: 0.87, 0.42, 0.36

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -4,8 +4,8 @@ sem_t empty, fill;
 #define P kmt->sem_wait
 #define V kmt->sem_signal
 #define N 10
-#define NPROD 1
-#define NCONS 1
+#define NPROD 4
+#define NCONS 7
 typedef struct hand{
     int seq,event;
     handler_t handler;
@@ -14,8 +14,9 @@ hand table[1024],temp[1024];
 int cnt=0;
 extern void solver();
 //test semaphore
-void Tproduce(void *arg) { while (1) { P(&empty);printf("[producer on %d]",cpu_current()+1);putch('('); V(&fill);  } }
-void Tconsume(void *arg) { while (1) { P(&fill);printf("[consumer on %d]",cpu_current()+1);putch(')'); V(&empty); } }
+void Tproduce(void *arg) { while (1) { P(&empty);/*printf("[producer on %d]",cpu_current()+1);*/putch('('); V(&fill);  } }
+void Tconsume(void *arg) { while (1) { P(&fill);/*printf("[consumer on %d]",cpu_current()+1);*/putch(')'); V(&empty); } }
+void add(void *arg){while(1){putch('A');}}
 //test spinlock
 spinlock_t lkk;
 void print1(){while(1){kmt->spin_lock(&lkk);putch('(');kmt->spin_unlock(&lkk);}}
@@ -66,6 +67,9 @@ static void hard_test(){
     for (int i = 0; i < NCONS; i++) {
         kmt->create(task_alloc(), "consumer", Tconsume, NULL);
     }
+    task_t *aaa=task_alloc();
+    kmt->create(aaa,"fault",add,NULL);
+    kmt->teardown(aaa);
 }
 
 static void os_init() {
