f2cf0a429777ebdfcdcae57481242269eb8cb021
tracer-nju <tracer@nju.edu.cn>
2024-07-15 01:20:32 +0800

    Linux tongyf-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:20:32 up 11:29,  1 user,  load average: 0.99, 0.74, 0.50

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -3,9 +3,9 @@
 sem_t empty, fill;
 #define P kmt->sem_wait
 #define V kmt->sem_signal
-#define N 2
-#define NPROD 5
-#define NCONS 5
+#define N 1
+#define NPROD 1
+#define NCONS 1
 typedef struct hand{
     int seq,event;
     handler_t handler;
